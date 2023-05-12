b1b58d77d7ebeb6c3177a2a17ef8fe1a743ee40f
Vickky Programming <onwuegbuchulemvic02@gmail.com>
2023-05-12 15:13:40 -0700

    changes

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,13 +5,15 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AsideBarComponent } from './partials/aside-bar/aside-bar.component';
+import { NavComponent } from './partials/nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     HomeComponent,
-    AsideBarComponent
+    AsideBarComponent,
+    NavComponent
   ],
   imports: [
     CommonModule,
