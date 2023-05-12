056ce0a41db4ef7d07262f674bda4515a71ab8df
Vickky Programming <onwuegbuchulemvic02@gmail.com>
2023-05-12 07:22:14 -0700

    Made Changes On the Admin Home Page Using Angular

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AsideBarComponent } from './partials/aside-bar/aside-bar.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import { HomeComponent } from './pages/home/home.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    HomeComponent,
-    AsideBarComponent
+    AsideBarComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 export class AdminModule { }
