cc883baf6d0ca4764aa7e1dd48c7ad7d0b848110
Vickky Programming <onwuegbuchulemvic02@gmail.com>
2023-05-13 05:04:32 -0700

    Added changes in admin portal

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,13 +5,15 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AsideBarComponent } from './partials/aside-bar/aside-bar.component';
+import { HeaderComponent } from './partials/header/header.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     HomeComponent,
-    AsideBarComponent
+    AsideBarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
