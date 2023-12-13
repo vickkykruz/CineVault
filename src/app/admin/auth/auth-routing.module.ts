9393d5cd83b72edf26f061bbf25fdb5e7ef3f464
VickkyProgrammer <73972088+vickkykruz@users.noreply.github.com>
2023-12-13 01:19:56 -0800

    Made Modifications

--- src/app/admin/auth/auth-routing.module.ts
+++ src/app/admin/auth/auth-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
-  { path: 'admin/auth/login', component: LoginComponent}
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/admin/auth/login/login.component.ts
+++ src/app/admin/auth/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Auth } from 'src/app/auth/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  userInfo: Auth = {
+    email: '',
+    password: ''
+  }
+
+  constructor (private title: Title) {}
+
+  ngOnInit(): void {
+      this.title.setTitle("CruzTv || Adminstartion Portal");
+  }
+
+  message!:string;
+
+  submitForm() {}
 }
